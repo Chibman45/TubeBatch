e38db5d4bcb40029a6a12956479630d56a393e90
Chibuzor collins <chibuzorcollins6@gmail.com>
2026-02-26 11:35:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8869689934-a9cf8",
+  "appId": "1:68418765937:web:34239ae6e6674eefb407d0",
+  "apiKey": "AIzaSyAP8Y05a8o1wcaKSd5nXAYV_wC8HlDwfck",
+  "authDomain": "studio-8869689934-a9cf8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "68418765937"
+};
